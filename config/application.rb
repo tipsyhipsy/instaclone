6ada422c1814a957e6b389976640ce47720c4735
setsuto horikawa <ralralph@gmail.com>
2019-08-18 16:26:09 +0900

    set timezonne: japan

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,16 @@ Bundler.require(*Rails.groups)
 
 module Instaclone
   class Application < Rails::Application
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+
+    config.generators do |g|
+      g.assets false
+      g.helper false
+    end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
